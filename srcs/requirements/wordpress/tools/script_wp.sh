f9eff079cbd9e290f2d1b87267f2836ee13c0c0e
Mmahias <mmahias@student.42.fr>
2023-02-18 17:22:20 +0100

    work...

--- srcs/requirements/wordpress/tools/script_wp.sh
+++ srcs/requirements/wordpress/tools/script_wp.sh
@@ -6,8 +6,10 @@ DB_NAME="wordpress"
 
 rm -rf wp-config.php
 echo "Downloading wp"
-wp-cli core download
+wp-cli --allow-root core download
 echo "Config wp"
-wp-cli config create --dbname=DB_NAME --dbuser=MY_USER --dbpass=MY_PWORD
+wp-cli --allow-root config create --dbname=DB_NAME --dbuser=MY_USER --dbpass=MY_PWORD
 echo "install"
-wp-cli core install --url='localhost' --title='Inception' --admin_user='mmahias' --admin_password='mworld' --admin_email='mmahias@42.fr'
+wp-cli --allow-root core install --url='localhost' --title='Inception' --admin_user='mmahias' --admin_password='mworld' --admin_email='mmahias@42.fr'
+
+exec php-fpm7.3 -F
