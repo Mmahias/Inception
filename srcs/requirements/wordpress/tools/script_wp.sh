0e0bcc25f5fa1101ceb014c95c1c8d83f8fc55bd
Mmahias <mmahias@student.42.fr>
2023-02-21 15:22:27 +0100

    in progress...

--- srcs/requirements/wordpress/tools/script_wp.sh
+++ srcs/requirements/wordpress/tools/script_wp.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
+rm -rf wp-config.php
 
-#rm -rf wp-config.php
-rm -rf /var/www/html/wp-config.php
-echo "Downloading wp"
-wp --allow-root core download
 echo "Config wp"
-wp --allow-root config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --path="/var/www/html"
+wp --allow-root core config --dbhost='db:3306' --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --skip-check
+sleep 4
 echo "install"
-wp --allow-root core install --url='localhost' --title='Inception' --admin_user='mmahias' --admin_password='mworld' --admin_email='mmahias@42.fr' --path="/var/www/html"
+wp --allow-root core install --url='localhost' --title='Inception' --admin_user='mmahias' --admin_password='mworld' --admin_email='mmahias@42.fr'
 
 exec php-fpm7.3 -F
