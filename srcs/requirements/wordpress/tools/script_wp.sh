cdeafb620a8630f508470e06a392e1491856c7ac
mmahias <mmahias@student.42.fr>
2023-02-24 12:36:55 +0100

    push finish

--- srcs/requirements/wordpress/tools/script_wp.sh
+++ srcs/requirements/wordpress/tools/script_wp.sh
@@ -5,6 +5,6 @@ rm -rf wp-config.php
 echo "Config wp"
 wp --allow-root core config --dbhost='mariadb' --dbname=$DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --skip-check
 echo "install"
-wp --allow-root core install --url='localhost' --title='Inception' --admin_user='mmahias' --admin_password='mworld' --admin_email='mmahias@42.fr'
+wp --allow-root core install --url='localhost' --title='Inception' --admin_user=$WP_AD_USER --admin_password=$WP_AD_PASSWORD --admin_email=$WP_AD_EMAIL
 
 exec php-fpm7.3 -F
